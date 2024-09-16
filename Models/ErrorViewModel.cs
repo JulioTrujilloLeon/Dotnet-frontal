9e88d14a0da9cc13e0ca0677286c01d6807ebad2
Julio Trujillo Leon <juliotrujilloleon@hotmail.es>
2024-09-16 13:19:40 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dotnet_frontal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
